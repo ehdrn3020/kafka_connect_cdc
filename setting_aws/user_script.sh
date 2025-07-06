082dac3c91c1e4a199f858957cdc5c3261d51697
ehdrn3020 <ehdrnsla3020@naver.com>
2025-07-07 00:35:11 +0900

    ec2 생성이후 필요 프로그램 설치 스크립트 작성

--- setting_aws/user_script.sh
+++ setting_aws/user_script.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-sudo yum groupinstall "Development Tools" -y && \
+# 개발 도구 설치
+sudo dnf groupinstall "Development Tools" -y && \
 sudo ln -s $(which python3) /usr/bin/python && \
 sudo python3 -m ensurepip --upgrade && \
 sudo python3 -m pip install --upgrade pip && \
-sudo python3 -m pip install packaging && \
-cd /opt && \
-wget https://files.pythonhosted.org/packages/source/a/ansible/ansible-2.9.27.tar.gz && \
-cd /usr/local && \
-sudo tar -xvzf /opt/ansible-2.9.27.tar.gz && \
-cd /usr/local/ansible-2.9.27 && \
-sudo make && \
-sudo make install && \
-sudo yum install git -y && \
-cd /home/ec2-user && \
+sudo python3 -m pip install packaging
+# ansible 설치
+cd /opt
+sudo wget https://files.pythonhosted.org/packages/source/a/ansible/ansible-2.9.27.tar.gz && \
+sudo tar -xvzf ansible-2.9.27.tar.gz && \
+cd ansible-2.9.27 && \
+sudo make
+sudo make install
+# git 설치
+sudo dnf install git -y
+# java 설치
+sudo dnf install -y java-17-amazon-corretto-devel
 
-
+# 홈 디렉토리로 이동
+cd /home/ec2-user
+git clone https://github.com/ehdrn3020/kafka_connect_cdc.git
